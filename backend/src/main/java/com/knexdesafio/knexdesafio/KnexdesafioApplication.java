4d16e5b6d5386ee705f792649811cb2d95f00e38
Paulocarneiroo <paulocarneirosegundo@gmail.com>
2025-06-19 19:51:14 -0300

    Projeto inicializado

---
+++ backend/src/main/java/com/knexdesafio/knexdesafio/KnexdesafioApplication.java
@@ -0,0 +1,13 @@
+package com.knexdesafio.knexdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnexdesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnexdesafioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/knexdesafio/knexdesafio/KnexdesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knexdesafio.knexdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnexdesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
